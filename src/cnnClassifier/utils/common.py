d4bd9b32cf2085860fc53a65c77527b6ddb1e3b2
felekekinfe <felekekinfe@gmail.com>
2024-12-06 16:37:22 +0300

    create_directory() added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -23,4 +23,38 @@ def read_yaml(yaml_path):
         raise ValueError('yaml file is empty')
     except Exception as e:
         return e
+@ensure_annotations
+def create_directory(directory_path:list,verbose=True):
 
+    for path in directory_path:
+        os.makedirs(path,exist_ok=True)
+
+        if verbose:
+            logger.inf(f'directory created at:{path}')
+
+
+@ensure_annotations
+def save_json(path,data):
+
+    with open(path,'w') as p:
+        json.dump(data,p,indent=4)
+        logger.info(f'json file created at{path}')
+
+@ensure_annotations
+def load_json(path):
+    with open(path,'r') as f:
+        data=json.load(f)
+    logger.info(f'json file loaded from{path}')
+    return ConfigBox(data)
+
+@ensure_annotations
+def decode_image(img_str,file_name):
+    img_data=base64.b64decode(img_str)
+    with open(file_name, 'wb') as f:
+        f.write(img_data)
+        f.close()
+@ensure_annotations
+def encode_image(cropped_img_path):
+    with open(cropped_img_path,'rb') as f:
+        encoded_img=base64.b64encode(f.read())
+    
