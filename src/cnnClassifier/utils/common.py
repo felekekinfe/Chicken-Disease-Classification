1b52d5dfd039c4ab54aae6677960439812bccec3
felekekinfe <felekekinfe@gmail.com>
2024-12-06 15:25:47 +0300

    read_yaml function added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -9,3 +9,18 @@ from box import ConfigBox
 from pathlib import Path
 from typing import Any
 import base64
+
+@ensure_annotations
+def read_yaml(yaml_path):
+
+    try:
+
+        with open(yaml_path,'r') as yaml_file:
+            data=yaml.safe_load(yaml_file)
+            logger.info(f'Reading yaml file from:{yaml_path}')
+            return ConfigBox(data)
+    except BoxValueError:
+        raise ValueError('yaml file is empty')
+    except Exception as e:
+        return e
+
