e0a182b5c4d4660476a480d4773081c28bac3731
felekekinfe <felekekinfe@gmail.com>
2024-12-08 00:28:31 +0300

    add chiken fecal image

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -11,7 +11,7 @@ from typing import Any
 import base64
 
 @ensure_annotations
-def read_yaml(yaml_path):
+def read_yaml(yaml_path:Path)-> ConfigBox:
 
     try:
 
@@ -22,16 +22,14 @@ def read_yaml(yaml_path):
     except BoxValueError:
         raise ValueError('yaml file is empty')
     except Exception as e:
-        return e
+        raise e
 @ensure_annotations
 def create_directory(directory_path:list,verbose=True):
 
     for path in directory_path:
         os.makedirs(path,exist_ok=True)
 
-        if verbose:
-            logger.inf(f'directory created at:{path}')
-
+        
 
 @ensure_annotations
 def save_json(path,data):
@@ -41,20 +39,8 @@ def save_json(path,data):
         logger.info(f'json file created at{path}')
 
 @ensure_annotations
-def load_json(path):
-    with open(path,'r') as f:
-        data=json.load(f)
-    logger.info(f'json file loaded from{path}')
-    return ConfigBox(data)
+def get_size(path):
 
-@ensure_annotations
-def decode_image(img_str,file_name):
-    img_data=base64.b64decode(img_str)
-    with open(file_name, 'wb') as f:
-        f.write(img_data)
-        f.close()
-@ensure_annotations
-def encode_image(cropped_img_path):
-    with open(cropped_img_path,'rb') as f:
-        encoded_img=base64.b64encode(f.read())
-    
+    size_in_kb=os.path.getsize(path)//1024
+
+    return f'{size_in_kb}KB'
