6ff30a5250e03bc6cb4721fa8381be2cabeea7ed
felekekinfe <felekekinfe@gmail.com>
2024-12-08 02:08:53 +0300

    data_ingestion.py created

---
+++ src/cnnClassifier/components/data_ingestion.py
@@ -0,0 +1,27 @@
+import os
+import urllib.request as request
+import zipfile
+from cnnClassifier import logger
+from cnnClassifier.utils.common import get_size
+from cnnClassifier.entity.config_entity import DataIngestionConfig
+
+
+class DataIngestion:
+    def __init__(self,config:DataIngestionConfig):
+        self.config=config
+    
+    def download_data(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename,header=request.urlretrieve(url=self.config.source_URL,filename=self.config.local_data_file)
+            logger.info(f'Downloaded {filename} with info:{header}')
+        else:
+            logger.info(f'file already exists with size:{get_size(Path(self.config.local_data_file))}')
+
+
+    def extract_zip_file(self):
+
+        unzip_path=self.config.unzip_dir
+        os.makedirs(unzip_path,exist_ok=True)
+
+        with zipfile.ZipFile(self.config.local_data_file,'r') as zip_file:
+            zip_file.extractall(unzip_path)
